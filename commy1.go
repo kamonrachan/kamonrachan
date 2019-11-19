6aa6a4d2b9565625555319a63c99e32d588c1570
kamonrachan <kamonrachan.to@rmuti.ac.th>
2019-11-19 22:52:10 +0700

    integers and floats.

--- commy1.go
+++ commy1.go
@@ -2,9 +2,8 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 	fmt.Println("kamonrachan" + "tokoksung")
-	fmt.Println("10+3 =" , 1+1)
-	fmt.Println("130/10 =" , 130/10)
+	fmt.Println("10+3 =", 1+1)
+	fmt.Println("130/10 =", 130/10)
 }
-
