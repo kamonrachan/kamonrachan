2e58847f0080389aaa0c51827dee401ec7f2c9ed
kamonrachan <kamonrachan.to@rmuti.ac.th>
2019-11-23 16:12:10 +0700

    commystart1

--- commy10.go
+++ commy10.go
@@ -5,3 +5,11 @@ import (
 	"strings"
 	"time"
 )
+func main() {
+	commystart1 := time.Now()
+	var a string
+	for i := 0; i < 50000; i++ {
+		a += "a"
+	}
+	fmt.Println("a" , time.Since(commystart1))
+}
