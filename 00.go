8ac80ad4223b3cc3cb81907b6b84502e31f048f2
kamonrachan <kamonrachan.to@rmuti.ac.th>
2019-11-23 16:09:29 +0700

    นำเข้า package "fmt" "strings" "time" มาใช้งาน

---
+++ 00.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"time"
+)
+
+func main() {
+	commystart1 := time.Now()
+	var a string
+	for i := 0; i < 50000; i++ {
+		a += "a"
+	}
+	fmt.Println("a" , time.Since(commystart1))
+
+	commystart2 := time.Now()
+	var b strings.Builder
+	for i := 0; i < 50000; i++ {
+		b.WriteString("b")
+	}
+	fmt.Println("b" , time.Since(commystart2))
+}
+
