21c743e5462569aec24622a30d44686399b52ab9
kamonrachan <kamonrachan.to@rmuti.ac.th>
2019-11-23 13:16:46 +0700

    Add command fmt.Println

--- commy3.go
+++ commy3.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print (`\n \t kamonrachan`)
-	
+	fmt.Print(`\n \t kamonrachan`)
+	fmt.Print("\n \t kamonrachan tokoksung")
+	fmt.Println()
+	fmt.Println(`\n \t kamonrachan`[0])
+	fmt.Println(len(`\n \t kamonrachan`))
+	fmt.Println("Hi" + "kamonrachan")
 }
