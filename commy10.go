5323d17a775a2bc6fe0f489d5d39781765510ac5
kamonrachan <kamonrachan.to@rmuti.ac.th>
2019-11-23 16:14:17 +0700

    commystart2

--- commy10.go
+++ commy10.go
@@ -5,11 +5,19 @@ import (
 	"strings"
 	"time"
 )
+
 func main() {
 	commystart1 := time.Now()
 	var a string
 	for i := 0; i < 50000; i++ {
 		a += "a"
 	}
-	fmt.Println("a" , time.Since(commystart1))
+	fmt.Println("a", time.Since(commystart1))
+
+	commystart2 := time.Now()
+	var b strings.Builder
+	for i := 0; i < 50000; i++ {
+		b.WriteString("b")
+	}
+	fmt.Println("b", time.Since(commystart2))
 }
