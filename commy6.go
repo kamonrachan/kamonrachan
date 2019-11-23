511da21a3fc8644c938e2536823b4687fc77dd16
kamonrachan <kamonrachan.to@rmuti.ac.th>
2019-11-23 15:03:20 +0700

    run

--- commy6.go
+++ commy6.go
@@ -3,10 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	var something bool=true
-	text:="kamonrachan"
-	age1,age2:=19,20
+	var something bool = true
+	text := "kamonrachan"
+	age1, age2 := 19, 20
 	fmt.Println("Value variable = ", something)
 	fmt.Println(text)
-	fmt.Println(age1,age2)
+	fmt.Println(age1, age2)
 }
