33351a2b4d71a4d836c6d73bbff958f3280a2381
kamonrachan <kamonrachan.to@rmuti.ac.th>
2019-11-23 14:18:00 +0700

    ดำเนินการทางคณิตศาสตร์

---
+++ commy5.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	NumberA := 10
+	NumberB := 3
+	fmt.Println(NumberA + NumberB)
+	fmt.Println(NumberA - NumberB)
+	fmt.Println(NumberA * NumberB)
+	fmt.Println(NumberA / NumberB)
+}
