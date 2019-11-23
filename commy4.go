1ad67fc6f12cb313ddcdc741f2753196ca61dcd0
kamonrachan <kamonrachan.to@rmuti.ac.th>
2019-11-23 13:54:01 +0700

    ภายใต้ fmt มีฟังก์ชัน Println ให้พิมพ์ข้อความออกจอได้

--- commy4.go
+++ commy4.go
@@ -5,5 +5,9 @@ import "fmt"
 func main() {
 	name := "com"
 	age := 19
-	
+	day := 29
+	year := 1999
+	fmt.Println("My name is", name)
+	fmt.Println("I am", age, "years old")
+	fmt.Println("My birthday is", day, "November", year)
 }
