8d0b08e721e126d191b1ae92ae004f3b10ef8473
kamonrachan <kamonrachan.to@rmuti.ac.th>
2019-11-23 15:46:02 +0700

    boilran

--- commy9.go
+++ commy9.go
@@ -1,6 +1,13 @@
 package main
 
+import "fmt"
+
 func main() {
 	kamonrachan := true
 	tokoksung := false
+	fmt.Println(kamonrachan)
+	fmt.Println(tokoksung)
+	number := 19 <= 20
+	fmt.Println(number)
+
 }
