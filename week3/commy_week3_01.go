2f01d1cb19578e797503b71f811a217a72a51736
kamonrachan <kamonrachan.to@rmuti.ac.th>
2019-11-30 16:02:50 +0700

    00

--- week3/commy_week3_01.go
+++ week3/commy_week3_01.go
@@ -1,9 +1,2 @@
 package main
 
-import "fmt"
-
-func main() {
-	n, e := fmt.Print("kamonrachan", "tokoksung", 013, 123, "\n")
-	fmt.Print("number of bytes written :", n, "\n")
-	fmt.Print("write error encountered :", e, "\n")
-} 
